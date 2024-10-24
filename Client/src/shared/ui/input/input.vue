3e9138ad7daa396192aa7e8ffe9986dd1b29bfcd
Edanriell <Edanriell@gmail.com>
2024-10-24 13:17:34 +0300

    feature(email-input): created email-input

--- Client/src/shared/ui/input/input.vue
+++ Client/src/shared/ui/input/input.vue
@@ -4,7 +4,7 @@
 		labelFor: string;
 		inputName: string;
 		inputId: string;
-		inputType: "text";
+		inputType: "text" | "email";
 	};
 
 	const { labelText, labelFor, inputName, inputId, inputType = "text" } = defineProps<InputProps>();
@@ -14,6 +14,10 @@
 	<div v-if="inputType === 'text'" class="text-input__field">
 		<label :for="labelFor" class="text-input__label">{{ labelText }} <span>*</span></label>
 		<input :id="inputId" :name="inputName" class="text-input" type="text" />
+	</div>
+	<div v-else-if="inputType === 'email'" class="email-input__field">
+		<label :for="labelFor" class="email-input__label">{{ labelText }} <span>*</span></label>
+		<input :id="inputId" :name="inputName" class="email-input" type="email" />
 	</div>
 </template>
 
@@ -52,5 +56,30 @@
 		font-size: 18rem;
 		line-height: 150%;
 		color: var(--color-grey-900);
+	}
+
+	.email-input__label {
+		display: flex;
+		flex-direction: row;
+		font-family: var(--font-family), sans-serif;
+		font-weight: 400;
+		font-size: 16rem;
+		line-height: 150%;
+		color: var(--color-grey-900);
+		column-gap: 8rem;
+
+		> span {
+			font-family: var(--font-family), sans-serif;
+			font-weight: 400;
+			font-size: 16rem;
+			line-height: 150%;
+			color: var(--color-green-600);
+		}
+	}
+
+	.email-input {
+		border: 1rem solid var(--color-grey-500);
+		border-radius: 8rem;
+		padding: 12rem 24rem;
 	}
 </style>
